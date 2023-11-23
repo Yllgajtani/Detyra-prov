4b8fb089b3ea5282cd7b08eaf41e22a089c8ac11
Yllgajtani <gajtaniyll@gmail.com>
2023-11-23 07:52:01 -0800

    Bday card

---
+++ script.js
@@ -0,0 +1,21 @@
+let titulli = document.getElementById('header')
+// let fotoja = document.querySelector('img')
+// let paragrafi1 = document.querySelector('.firstP')
+// let paragrafi2 = document.querySelector('.secondP')
+// let from = document.getElementById('from')
+
+// titulli.textContent = 'Urime ditlindjen! 🍰'
+// fotoja.src = 'https://img.freepik.com/free-vector/detailed-birthday-lettering-with-balloons_52683-57605.jpg'
+// fotoja.alt = 'Foto e insertuar me javascript'
+// paragrafi1.textContent = 'Urime ditlindjen edhe 💯 tjera me gjith kend deshiron ti! 😆'
+// from.textContent = 'Nga Yll Gajtani!'
+// paragrafi2.textContent = 'Ishalla te pletsohen te gjitha andrrat !'
+
+titulli.style.fontStyle = 'italic'
+titulli.style.color = 'green'
+titulli.style.backgroundColor = 'yellow'
+titulli.style.textDecoration = 'underline'
+
+titulli.classList.add('II')
+titulli.classList.remove('II')
+
